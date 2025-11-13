8906fbfbc677801a51ae12d786a73e802552f2e9
Winston <Winstonrodrii@gmail.com>
2025-11-13 11:50:37 -0500

    feat: more test cases was uploaded

--- test_calculadora.py
+++ test_calculadora.py
@@ -11,26 +11,24 @@ driver.get('file:///Users/rosdri/Documents/PruebaAceptacion/PruebaAceptacion/cal
 
 
 # Se encuentran los botones y se envian los datos
-driver.find_element(By.ID, 'numero1').send_keys('9')
-driver.find_element(By.ID, 'numero2').send_keys('5')
-# se usa XPTAH ya que se debe buscar el nombre por la funcion debido a que hay mas de un boton ygit remote add origin https://<TOKEN>@github.com/WinstonR0/PruebaAceptacion.git
+driver.find_element(By.ID, 'numero1').send_keys('-9')
+driver.find_element(By.ID, 'numero2').send_keys('5.10')
 
 
-#Se espera a que el boton sea clickeable
 # Se debe poner el texto dentro del boton, no como tal el ID
 driver.find_element(By.XPATH, "//button[contains(text(), 'Suma')]").click()
 
 # Se debe esperar la respuesta
-time.sleep(3)
+time.sleep(2)
 resultado = driver.find_element(By.ID,'resultado').text.strip() # Strip para quitar espacios
 
 # Se hace el test
 #PRIMERA FORMA
 
 if '15' in resultado:
-    print ("Prueba de aceptacion superada")
+    print ("Prueba #1 suma superada")
 else:
-    print("Prueba de aceptacion fallida", resultado)
+    print("Prueba #1 suma fallida", resultado)
 
 '''
 SEGUNDA FORMA
@@ -41,7 +39,70 @@ else:
     print("Prueba de aceptacion fallida", resultado)
 '''
 
+# Resta
+# Se refresca la pagina
+driver.refresh()
 
+time.sleep(2)
+driver.find_element(By.ID, "numero1").send_keys('10')
+driver.find_element(By.ID, "numero2").send_keys('-10')
+
+driver.find_element(By.XPATH, "//button[contains(text(), 'Resta')]").click()
+
+time.sleep(2)
+resultado = driver.find_element(By.ID,'resultado').text.strip() # Strip para quitar espacios
+
+# Se hace el test
+
+if '5' in resultado:
+    print ("Prueba #2 resta superada")
+else:
+    print("Prueba #2 resta fallida", resultado)
+
+
+# Multiplicacion
+# Se refresca la pagina
+driver.refresh()
+
+time.sleep(2)
+
+driver.find_element(By.ID, "numero1").send_keys('100')
+driver.find_element(By.ID, "numero2").send_keys('0')
+
+driver.find_element(By.XPATH, "//button[contains(text(), 'Multiplicacion')]").click()
+
+time.sleep(2)
+resultado = driver.find_element(By.ID,'resultado').text.strip() # Strip para quitar espacios
+
+# Se hace el test
+
+if '500' in resultado:
+    print ("Prueba #3 Multiplicacion superada")
+else:
+    print("Prueba #3 Multiplicacion fallida", resultado)
+
+
+
+# Division
+# Se refresca la pagina
+driver.refresh()
+
+time.sleep(2)
+
+driver.find_element(By.ID, "numero1").send_keys('-49218273')
+driver.find_element(By.ID, "numero2").send_keys('50,817373')
+
+driver.find_element(By.XPATH, "//button[contains(text(), 'Division')]").click()
+
+time.sleep(2)
+resultado = driver.find_element(By.ID,'resultado').text.strip() # Strip para quitar espacios
+
+# Se hace el test
+
+if '500' in resultado:
+    print ("Prueba #4 Division superada")
+else:
+    print("Prueba #4 Divison fallida", resultado)
 
 # Se cierra el navegador
-driver.quit()
+#driver.quit()
